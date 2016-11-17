3f5e02dbbd3f2f3c15c632bd3ac9ec334f8ac808
Saket <skate056@gmail.com>
2016-11-17 16:44:39 +0000

    Reformat

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,11 +1,12 @@
 object Versions {
   val Avro = "1.7.7"
+  val AwsSdk = "1.9.6"
+  val AwsSqsJms = "1.0.0"
   val Kafka = "0.10.0.1"
+  val CommonLogging = "1.2"
   val Confluent = "3.0.0"
+  val Logback = "1.1.7"
   val ScalaCheck = "1.13.4"
   val ScalaLogging = "3.5.0"
   val ScalaTest = "3.0.0"
-  val AwsSdk = "1.9.6"
-  val AwsSqsJms = "1.0.0"
-  val Logback = "1.1.7"
 }

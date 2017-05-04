5af4d5d4b33b1cc434500ce6ad33a264b2ed7120
R Vadai <rvadai@segence.com>
2017-05-04 11:56:42 +0100

    Upgrading to Kafka 0.10.2.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   val Avro = "1.7.7"
   val AwsSdk = "1.9.6"
   val AwsSqsJms = "1.0.0"
-  val Kafka = "0.10.0.1"
+  val Kafka = "0.10.2.0"
   val CommonLogging = "1.2"
   val Confluent = "3.0.0"
   val ScalaCheck = "1.13.4"
